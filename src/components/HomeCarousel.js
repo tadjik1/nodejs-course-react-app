277a3c18d21ceb6599ee256361514428905ce0e6
hermansemykozov <semikozovgerman@gmail.com>
2019-06-12 22:33:13 +0300

    CHANGE: wrap HomeCarousel as reactstrap component

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,71 +1,119 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function HomeCarousel() {
-  return (
-    <div id="mainCarousel" className="main-carousel carousel slide">
-      <ol className="carousel-indicators">
-        <li data-target="#mainCarousel" data-slide-to="0" className="active"></li>
-        <li data-target="#mainCarousel" data-slide-to="1"></li>
-        <li data-target="#mainCarousel" data-slide-to="2"></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="assets/images/default-slide-img.jpg" alt="First slide" />
-          <div className="container">
-            <div className="carousel-caption">
-              <h3 className="h1">BEST LAPTOP DEALS</h3>
-              <div>
-                <a className="btn" href="#" role="button">
-                  View all DEALS
-                  <img src="assets/icons/icon-angle-white.svg" className="ml-3" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img className="second-slide" src="assets/images/default-slide-img.jpg"
-               alt="Second slide" />
-          <div className="container">
-            <div className="carousel-caption">
-              <h3 className="h1">BEST LAPTOP DEALS</h3>
-              <div>
-                <a className="btn" href="#" role="button">
-                  View all DEALS
-                  <img src="assets/icons/icon-angle-white.svg" className="ml-3" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img className="third-slide" src="assets/images/default-slide-img.jpg"
-               alt="Third slide" />
-          <div className="container">
-            <div className="carousel-caption">
-              <h3 className="h1">BEST LAPTOP DEALS</h3>
-              <div>
-                <a className="btn" href="#" role="button">
-                  View all DEALS
-                  <img src="assets/icons/icon-angle-white.svg" className="ml-3" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a className="carousel-control-prev" href="#mainCarousel" role="button"
-         data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#mainCarousel" role="button"
-         data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-  );
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators
+} from 'reactstrap';
+
+const items = [
+    {
+        id: '0',
+        src: 'assets/images/default-slide-img.jpg',
+        altText: 'Slide 1',
+        captionTitle: 'BEST LAPTOP DEALS',
+        captionBtnText: 'View all DEALS',
+        captionBtnLink: '#',
+        captionBtnIconSrc: 'assets/icons/icon-angle-white.svg'
+    },
+    {
+        id: '1',
+        src: 'assets/images/default-slide-img.jpg',
+        altText: 'Slide 2',
+        captionTitle: 'BEST LAPTOP DEALS',
+        captionBtnText: 'View all DEALS',
+        captionBtnLink: '#',
+        captionBtnIconSrc: 'assets/icons/icon-angle-white.svg'
+    },
+    {
+        id: '2',
+        src: 'assets/images/default-slide-img.jpg',
+        altText: 'Slide 3',
+        captionTitle: 'BEST LAPTOP DEALS',
+        captionBtnText: 'View all DEALS',
+        captionBtnLink: '#',
+        captionBtnIconSrc: 'assets/icons/icon-angle-white.svg'
+    }
+];
+
+class HomeCarousel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+    }
+
+    render() {
+        const { activeIndex } = this.state;
+
+        const slides = items.map((item) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.id}
+                >
+                    <img src={item.src} alt={item.altText} className="third-slide" />
+                    <div className="container">
+                        <div className="carousel-caption">
+                            <h3 className="h1">{ item.captionTitle }</h3>
+                            <div>
+                                <a className="btn" href={ item.captionBtnLink } role="button">
+                                    { item.captionBtnText }
+                                    <img src={ item.captionBtnIconSrc } className="ml-3" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </CarouselItem>
+            );
+        });
+
+        return (
+            <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}
+                className="main-carousel"
+                ride={false}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
+        );
+    }
 }
 
 export default HomeCarousel;
